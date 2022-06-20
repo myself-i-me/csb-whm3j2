539e29ef62cb052eb442a9cca9f24be1fa76a574
Code Sandbox <hello@codesandbox.io>
2022-06-20 03:39:49 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.35cef27494e3fca5abab3faf1104a176.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb24fe1d0d36ee4437d3a7692ec97646",
+    "url": "https://myself-i-me.github.io/csb-whm3j2/index.html"
+  },
+  {
+    "revision": "71f045f848a6ac20640c",
+    "url": "https://myself-i-me.github.io/csb-whm3j2/static/css/main.10d6a708.chunk.css"
+  },
+  {
+    "revision": "fcc7e9104c80fcaa0b7a",
+    "url": "https://myself-i-me.github.io/csb-whm3j2/static/js/2.c9335df9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "https://myself-i-me.github.io/csb-whm3j2/static/js/2.c9335df9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71f045f848a6ac20640c",
+    "url": "https://myself-i-me.github.io/csb-whm3j2/static/js/main.39abbe3b.chunk.js"
+  },
+  {
+    "revision": "18dcd1a6e12d1c5fa9b5",
+    "url": "https://myself-i-me.github.io/csb-whm3j2/static/js/runtime-main.70bde13c.js"
+  }
+]);
